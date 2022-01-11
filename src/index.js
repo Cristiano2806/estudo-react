71f484c072d7227698be09e2e8303d518775d552
Cristiano2806 <cristiano.botelho@sou.unifeob.edu.br>
2022-01-10 21:24:37 -0300

    subindo aplicacaoo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import ReactDom from 'react-dom'
+import React from 'react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import Primeiro from './components/basicos/Primeiro'
+
+ReactDom.render(
+    <div>
+        <Primeiro></Primeiro>
+    </div>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
