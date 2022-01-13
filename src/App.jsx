8131e61b1953cb92d2988399feb60669f5761805
Cristiano2806 <cristiano.botelho@sou.unifeob.edu.br>
2022-01-13 01:15:55 -0300

    Subindo estilizações iniciais e cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
+import './App.css';
 import React from "react";
 
-import Aleatorio from "./components/basicos/Aleatorio"
+import Card from "./components/layout/Card";
+import Aleatorio from "./components/basicos/Aleatorio";
 import Fragmento from "./components/basicos/Fragmento";
 import ComParametro from "./components/basicos/ComParametro";
 import Primeiro from "./components/basicos/Primeiro";
 
 export default () => (
-  <div id="app">
-    <h1>Fundamentos React (Arrow _)</h1>
-    <Fragmento/>
-    <ComParametro titulo="Situação do Aluno" 
-    aluno="Pedro Cássio" nota={9.6} />
-    <Primeiro></Primeiro>
-    <Aleatorio min={1} max={60}/>
+  <div className="App">
+    <h1>Fundamentos React</h1>
+
+    <div className="Cards">
+      <Card titulo="#04 - Desafio Aleatório">
+        <Aleatorio min={1} max={60} />
+      </Card>
+
+      <Card titulo="#03 - Fragmento">
+        <Fragmento />
+      </Card>
+
+      <Card titulo="#02 - Com Parâmetro">
+        <ComParametro
+          titulo="Situação do Aluno"
+          aluno="Pedro Cássio"
+          nota={9.6}
+        />
+      </Card>
+
+      <Card titulo="#01 - Primeiro Componente">
+        <Primeiro></Primeiro>
+      </Card>
+    </div>
   </div>
 );
